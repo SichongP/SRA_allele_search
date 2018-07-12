938391fc3ecf3f40ef3fdc5b33b580bf7614a23d
Sichong Peng <scpeng@ucdavis.edu>
2018-07-12 15:05:05 -0700

    now recognizes different chr name style

--- bin/read_pileup.py
+++ bin/read_pileup.py
@@ -17,33 +17,36 @@ import os, os.path
 import sys, traceback
 import csv
 import re
+import pprint
 def read_file(root, filename, varianthash, refhash):
 	"""
 	read data from a file and store into a hash
 	"""
 	genebank_ids = {}
 	refseq_ids = {}
-	for line in open("chr_equcab2"):
+	for line in open(os.path.join(os.path.dirname(os.path.abspath(__file__)),"chr_equcab2")):
 		chr, genebank, refseq = line.strip().split('\t',3)
 		genebank_ids[genebank] = chr
 		refseq_ids[refseq] = chr
-	for line in open("chr_equcab3"):
+	for line in open(os.path.join(os.path.dirname(os.path.abspath(__file__)),"chr_equcab3")):
 		chr, genebank, refseq = line.strip().split('\t',3)
 		genebank_ids[genebank] = chr
 		refseq_ids[refseq] = chr
+#	print(genebank_ids)
 	for line in open(os.path.join(root, filename)):
-		print(line)
+#		print(line)
 		chr, pos, ref, depth, match, a, c, g, t, ins, dele, something = line.strip().split('\t',11)
 		a=re.sub('-A','',a)
 		c=re.sub('-C','',c)
 		g=re.sub('-G','',g)
 		t=re.sub('-T','',t)
-		if chr in geneban_ids:
+		if chr in genebank_ids:
 			identifier=genebank_ids[chr]+','+pos
 		elif chr in refseq_ids:
 			identifier=refseq_ids[chr]+','+pos
 		else:
 			identifier=chr+','+pos
+#		print(identifier)
 		refhash[identifier] = ref
 		if identifier not in varianthash:
 			varianthash[identifier] = {}
@@ -92,13 +95,13 @@ def read_file(root, filename, varianthash, refhash):
 def main():
 	varianthash={}
 	refs={}
-	start_dir = './temp_pileup_2018-07-02'
+	start_dir = './temp_pileup_2018-06-27'
 	print('Starting in:', os.path.abspath(start_dir))
 	for root, dirs, files in os.walk(start_dir):
 		for filename in files:
-			print(filename)
+#			print(filename)
 			read_file(root, filename, varianthash, refs)
-	print(varianthash)
+	pprint.pprint(varianthash)
 
 if __name__ == "__main__":
 	main()
